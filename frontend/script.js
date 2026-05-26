d4219c1a2c11911257b9d205e48f253b213fa610
Dhavi <dhaviafif@gmail.com>
2026-05-26 12:52:57 +0700

    fix https

--- frontend/script.js
+++ frontend/script.js
@@ -19,7 +19,7 @@ async function generateTopic() {
     try {
 
         const response = await fetch(
-            "http://skripsi-topic-finder-production.up.railway.app",
+            "skripsi-topic-finder-production.up.railway.app",
             {
 
                 method: "POST",
