5c94c1d9908012730615d1017c2f4e4dc237fa26
Dhavi <dhaviafif@gmail.com>
2026-05-26 12:29:50 +0700

    connect frontend to railway

--- frontend/script.js
+++ frontend/script.js
@@ -19,7 +19,7 @@ async function generateTopic() {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:5000/generate",
+            "http://skripsi-topic-finder.railway.internal/generate",
             {
 
                 method: "POST",
